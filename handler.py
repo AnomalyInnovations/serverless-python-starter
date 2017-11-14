8126c199f0393d130682bed9f189975dd37b4245
Frank <frank@seed.run>
2017-11-14 16:02:06 -0500

    Removed numpy dependency

--- handler.py
+++ handler.py
@@ -1,11 +1,8 @@
 import json
-import numpy as np
 
 def hello(event, context):
-  array = np.arange(15).reshape(3, 5)
-  array_str = ','.join(str(x) for x in array)
   body = {
-    "message": "Python numpy example: " + array_str,
+    "message": "Go Serverless v1.0! Your function executed successfully!",
   }
 
   return {
--- tests/test_handler.py
+++ tests/test_handler.py
@@ -7,4 +7,4 @@ class HandlerTest(unittest.TestCase):
   def test_event_failsWithNumberAsEvent(self):
     response = hello(1, 2)
     self.assertEqual(response.get('statusCode'), 200)
-    self.assertTrue(isinstance(response.get('body'), str), 200)
+    self.assertTrue(isinstance(response.get('body'), str))
